f80f2793816cdd7cbff0525af74031976f9ab042
violacathrine <cathrineohlsson@live.se>
2025-02-28 09:24:09 +0100

    small changes

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 const filterGroup = document.getElementById("filter-group")
 const filterButtons = filterGroup.querySelectorAll(".filter-btn")
 const sortGroup = document.getElementById("sort-group")
-const sortButtons = sortGroup.querySelectorAll(".sort-btn")
+const sortButtons = sortGroup.querySelectorAll(".sort-btn") 
 const placeholderBox = document.getElementById("placeholder-box")
+const messageList = document.getElementById("messageList")
 const recipeImage = document.getElementById("recipe-image")
 const recipeContent = document.getElementById("recipe-content")
 
 // Different messages when specific button is clicked
+const maxMessages = 8
 const messages = {
     all: "You are very hungry huh?",
     italy: "Want to make a pizza maybe?",
@@ -40,12 +42,17 @@ sortButtons.forEach(button =>{
 document.querySelectorAll(".filter-btn, .sort-btn").forEach(button =>
     button.addEventListener("click", () => {
 const newMessage = messages[button.dataset.filter] || 
-messages[button.dataset.sort] || "No matching filter found."
+      messages[button.dataset.sort] || 
+      "No matching filter found."
 
 const paragraph = document.createElement("p")
 paragraph.innerText = newMessage
+messageList.appendChild(paragraph)
 
-placeholderBox.appendChild(paragraph)
+ if (messageList.children.length > maxMessages) {
+    messageList.removeChild(messageList.firstChild)
+}
+
     })
 )
 
